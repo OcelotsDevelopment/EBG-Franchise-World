50b417ed71d238e5325eda4fbabe0405bc5f5fdc
robinOcelots <robin@ocelots.in>
2025-07-04 12:52:11 +0530

    Mobile responsive

--- src/main.js
+++ src/main.js
@@ -4,3 +4,15 @@ import '@fontsource-variable/roboto';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './oppor'
 // import './heroAnimation'
+import './mobileMenu'
+
+document.querySelectorAll('a[href^="#"]').forEach(elem => {
+    elem.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(elem.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            offsetTop: 20
+        });
+    });
+});
+
